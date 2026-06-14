4750474191afefaec19753e83e143bd9c6b0c72b
Rishi-11-26 <rishipourusha@gmail.com>
2026-06-14 13:17:02 +0530

    Update firebase.ts

--- Xenolyth/src/lib/firebase.ts
+++ Xenolyth/src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1VfXU3_-83dBc6Vdg1sTEiZDTpju6eJo",
-  authDomain: "xenolyth-f6baa.firebaseapp.com",
-  projectId: "xenolyth-f6baa",
-  storageBucket: "xenolyth-f6baa.firebasestorage.app",
-  messagingSenderId: "253188212810",
-  appId: "1:253188212810:web:7a64aae2503d60945cfeb3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase only if it hasn't been initialized yet
